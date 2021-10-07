e6bc340d6b9f3b9af5e2cafb711bda03e4fc45c8
SAER93 <romanpechegin@gmail.com>
2021-10-07 21:46:36 +0500

    Сделал layout для fragment_auth.xml

--- app/src/main/java/com/example/testappforwork/ui/auth/AuthFragment.kt
+++ app/src/main/java/com/example/testappforwork/ui/auth/AuthFragment.kt
@@ -1,16 +1,24 @@
 package com.example.testappforwork.ui.auth
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import com.example.testappforwork.R
 import com.example.testappforwork.databinding.FragmentAuthBinding
+import com.example.testappforwork.utilities.hideKeyboard
 
 class AuthFragment : Fragment(R.layout.fragment_auth) {
     private var _binding: FragmentAuthBinding? = null
     private val binding get() = _binding!!
+
+    private val emailPattern = Regex(EMAIL_PATTERN)
+    private val passwordPattern = Regex(PASSWORD_PATTERN)
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,8 +29,29 @@ class AuthFragment : Fragment(R.layout.fragment_auth) {
         return binding.root
     }
 
+    override fun onStart() {
+        super.onStart()
+
+        binding.loginButton.setOnClickListener {
+            val email = binding.loginEditText.text.toString().trim()
+            val password = binding.passwordEditText.text.toString().trim()
+            if (email.matches(emailPattern) && email.isNotEmpty()
+                && password.matches(passwordPattern) && password.isNotEmpty()) {
+                hideKeyboard()
+                binding.illegalLoginOrPassword.visibility = View.GONE
+            } else {
+                binding.illegalLoginOrPassword.visibility = View.VISIBLE
+            }
+        }
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    companion object {
+        const val EMAIL_PATTERN = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"
+        const val PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{6,}\$"
     }
 }
--- app/src/main/java/com/example/testappforwork/utilities/funs.kt
+++ app/src/main/java/com/example/testappforwork/utilities/funs.kt
@@ -1,6 +1,10 @@
 package com.example.testappforwork.utilities
 
+import android.content.Context
+import android.view.inputmethod.InputMethodManager
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.bumptech.glide.Glide
@@ -18,4 +22,10 @@ fun ImageView.downloadAndSetImage(url: String) {
                 .override(500, 500)
                 .into(this)
     }
+}
+
+fun Fragment.hideKeyboard() {
+    val inputMethodManager =
+        requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    inputMethodManager.hideSoftInputFromWindow(requireActivity().window.decorView.windowToken, 0)
 }
